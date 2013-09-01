bcf9edc836d284bf3a747f21cdf9f6032cf3b335
zero13cool <zero13cool@yandex.ru>
2013-09-01 16:58:48 +0400

    fix relative module import

--- backends/__init__.py
+++ backends/__init__.py
@@ -4,7 +4,7 @@ import os
 
 # auto import all backends from `backends` package
 package_dirname = 'backends'
-for filaneme in os.listdir(package_dirname):
+for filaneme in os.listdir(os.path.dirname(__file__)):
     if filaneme.endswith('.pyc') or \
             filaneme.startswith('__'):
         continue
